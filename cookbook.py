2056d2c7b67f917f8c950dcdd74d6b5445e8b060
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-09 13:42:22 +0100

    Task detailled

--- cookbook.py
+++ cookbook.py
@@ -1,1 +1,7 @@
-print("Welcome to my cookbook")
+# Create an application to store and retrieve recipes.
+# Each recipe can include a name, list of ingredients, quantities, and cooking instructions.
+# Practice Skills:
+# Use a dictionary to map recipe names to their details.
+# Ingredients can be stored in tuples (ingredient, quantity) within a list.
+# Practice adding new recipes, updating existing ones, and retrieving a recipe by name.
+# Use slicing and string methods for displaying and formatting recipes.
