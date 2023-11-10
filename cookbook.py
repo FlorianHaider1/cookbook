809ecbe02ba7ac01cb0da7a217c7ae421288a92c
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-10 22:44:26 +0100

    Update cookbook.py

--- cookbook.py
+++ cookbook.py
@@ -35,20 +35,32 @@ def open_cookbook(book):
     navigation()
 
 def navigation():
-    control = str(input("(N)ew Entry?"))
+    control = str(input("(N)ew entry?\t(D)elete entry\t"))
     if control.upper() == "N":
         new_entry()
+    elif control.upper() == "D":
+        delete = str(input("Which entry do you want to delete? (Z)urück\t"))
+        if delete.upper() != "Z":
+            delete_check = str(input(f"Are you sure you want to delete {delete}? (Y)es or (N)o\t"))
+            if delete_check.upper() == "Y":
+                cookbook.pop(delete)
+                open_cookbook(cookbook)
+            if delete_check.upper() == "N":
+                open_cookbook(cookbook)
+        if delete.upper() == "Z":
+            open_cookbook(cookbook)
+
 
 def new_entry():
-    new_dish = "Spaghetti Bolognese"
+    new_dish = str(input("Add new dish:\t"))
     cookbook[new_dish] = {}
     x = 1
     while True:
         print(new_dish)
-        new_ingredient = str(input(f"Please enter ingredient {x}: "))
-        new_quantitiy = int(input(f"Please enter Quantity for {new_ingredient}: "))
+        new_ingredient = str(input(f"Please enter ingredient {x}:\t"))
+        new_quantitiy = int(input(f"Please enter Quantity for {new_ingredient}:\t"))
         cookbook[new_dish][new_ingredient] = [new_quantitiy]
-        control = str(input("(N)ext ingredient\n(S)ave and Exit\n(E)xit without saving"))
+        control = str(input("(N)ext ingredient\n(S)ave and Exit\n(E)xit without saving\t"))
         if control.upper() == "N":
             x += 1
             continue
@@ -59,4 +71,3 @@ def new_entry():
             open_cookbook(cookbook)
 
 open_cookbook(cookbook)
-new_entry()
